0df79f05935e0d38bedff32ca1b92f2f673b2c26
Josh Lauricha <josh@lauricha.com>
2026-01-21 16:23:03 -0800

    init

---
+++ stevebot.ino
@@ -0,0 +1,64 @@
+// SPDX-FileCopyrightText: 2022 Limor Fried for Adafruit Industries
+//
+// SPDX-License-Identifier: MIT
+
+#include "WiFi.h"
+#include <Adafruit_TestBed.h>
+extern Adafruit_TestBed TB;
+
+#define RELAY_PIN 13
+
+// the setup routine runs once when you press reset:
+void setup() {
+  Serial.begin(115200);
+
+  // enable the relay pin
+  pinMode(RELAY_PIN, OUTPUT);
+  digitalWrite(RELAY_PIN, HIGH);
+
+  // Set WiFi to station mode and disconnect from an AP if it was previously connected
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+}
+
+// the loop routine runs over and over again forever:
+uint8_t loopCounter = 0;
+void loop() {
+  if (loopCounter == 0) {
+    // Test I2C!
+    Serial.print("I2C port ");
+    TB.theWire = &Wire;
+    TB.printI2CBusScan();
+
+    // Test WiFi Scan!
+    // WiFi.scanNetworks will return the number of networks found
+    int n = WiFi.scanNetworks();
+    Serial.print("WiFi AP scan done...");
+    if (n == 0) {
+      Serial.println("no networks found");
+    } else {
+      Serial.print(n);
+      Serial.println(" networks found");
+      for (int i = 0; i < n; ++i) {
+        // Print SSID and RSSI for each network found
+        Serial.print(i + 1);
+        Serial.print(": ");
+        Serial.print(WiFi.SSID(i));
+        Serial.print(" (");
+        Serial.print(WiFi.RSSI(i));
+        Serial.print(")");
+        Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN) ? " " : "*");
+        delay(10);
+      }
+    }
+    Serial.println("");
+
+    // turn the relay pin on
+    digitalWrite(RELAY_PIN, HIGH);
+  } else if(loopCounter == 128) {
+    digitalWrite(RELAY_PIN, LOW);
+  }
+
+  delay(100);
+  loopCounter++;
+}
